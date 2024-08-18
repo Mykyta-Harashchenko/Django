d0123ca4d11ef642fa5340f5b9d84ebdcdfc72c8
Mykyta-Harashchenko <garasenkonikita538@gmail.com>
2024-08-18 11:39:53 +0100

    First try

--- hw_project/app_auth/urls.py
+++ hw_project/app_auth/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 from . import views
 from .forms import LoginForm, RegisterForm
@@ -12,4 +12,8 @@ urlpatterns = [
     path('signin/', LoginView.as_view(template_name='app_auth/login.html', form_class=LoginForm), name='signin'),
     path('logout/', views.logoutuser, name='logoutuser'),
     path('profile/', views.profile, name='profile'),
+    path('reset_password/', PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
