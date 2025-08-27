bb48bf8b8d72a05431f4ec54d5b1ec8b02ace735
Mykyta-Harashchenko <garasenkonikita538@gmail.com>
2025-08-27 16:24:47 +0100

    Added/Admin/System

--- hw_project/core/app_auth/admin.py
+++ hw_project/core/app_auth/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Profile
+from core.app_auth.models import User, Profile
 
 # Register your models here.
-admin.site.register(Profile)
+    
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user__username',)
+    
+    
